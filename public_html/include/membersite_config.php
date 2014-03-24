088a9ae427808f2f195c16ad17d4cc08160edfad
ikeuli <m0ixbear@live.ca>
2014-03-25 00:52:42 +0900

    user registration works but needs to be changed

--- public_html/include/membersite_config.php
+++ public_html/include/membersite_config.php
@@ -4,23 +4,23 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('heunjeok.com');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('contact@heunjeok.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
-                      /*table name*/'fgusers3');
+                      /*username*/'heuncxep',
+                      /*password*/'fadfjke11',
+                      /*database name*/'heuncxep_heunjeok',
+                      /*table name*/'users3');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
-$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+$fgmembersite->SetRandomKey('x9swNLo5coNL0pw');
 
 ?>
