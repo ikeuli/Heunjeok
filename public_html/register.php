b1a46c568f14d37413a72a95d2e201b3c7de73f6
ikeuli <m0ixbear@live.ca>
2014-05-09 10:28:38 +0900

    unbroke things

--- public_html/register.php
+++ public_html/register.php
@@ -10,36 +10,33 @@ if(isset($_POST['submitted']))
 }
 
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
-
 <head>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
     <title>Heunjeok | Sign Up</title>
     <link rel="STYLESHEET" type="text/css" href="css/fg_membersite.css" />
     <script type='text/javascript' src='js/gen_validatorv31.js'></script>
     <link rel="STYLESHEET" type="text/css" href="css/pwdwidget.css" />
-    <script src="js/pwdwidget.js" type="text/javascript"></script>      
+    <script src="js/pwdwidget.js" type="text/javascript"></script>  
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
 </head>
-
 <body background="http://huntsy.com/assets/blue_thatch.png">
 	<img src="/assets/images/heunjeok.png" id="logo" alt="Heunjeok logo">
-		<ul id="nav_container">
-            <li>
-                <a href="index.html">Home</a> 
-            </li>
-            <li>
-                <a href="register.php">Sign Up</a> 
-            </li>
-            <li>
-                <a href="login.html">Log In</a> 
-            </li>
-            <li>
-                <a href="contact.html">Contact</a> 
-            </li>
-        </ul>
+	<ul id="nav_container">
+		<li>
+			<a href="index.html">Home</a>
+		</li>
+		<li>
+			<a href="register.php">Sign Up</a>
+		</li>
+		<li>
+			<a href="login.html">Log In</a>
+		</li>
+		<li>
+			<a href="contact.html">Contact</a>
+		</li>
+	</ul>
 
 <!-- Form Code Start -->
 <div id="main_content" align="center">
@@ -54,6 +51,16 @@ if(isset($_POST['submitted']))
 <input type='text'  class='spmhidip' name='<?php echo $fgmembersite->GetSpamTrapInputName(); ?>' />
 
 <div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='name' >Your Full Name*: </label><br/>
+    <input type='text' name='name' id='name' value='<?php echo $fgmembersite->SafeDisplay('name') ?>' maxlength="50" /><br/>
+    <span id='register_name_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='email' >Email Address*:</label><br/>
+    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='register_email_errorloc' class='error'></span>
+</div>
 <div class='container'>
     <label for='username' >UserName*:</label><br/>
     <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
@@ -71,10 +78,11 @@ if(isset($_POST['submitted']))
 <div class='container'>
     <input type='submit' name='Submit' value='Submit' />
 </div>
-</div>
 
 </fieldset>
 </form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
 
 <img src="/assets/images/dollarswon.png" alt="Dollar and won sign">
 
@@ -86,6 +94,11 @@ if(isset($_POST['submitted']))
     var frmvalidator  = new Validator("register");
     frmvalidator.EnableOnPageErrorDisplay();
     frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("name","req","Please provide your name");
+
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
 
     frmvalidator.addValidation("username","req","Please provide a username");
     
@@ -93,5 +106,10 @@ if(isset($_POST['submitted']))
 
 // ]]>
 </script>
+
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
 </body>
 </html>
