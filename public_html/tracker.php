8937a82417aa94fcae24bac5216d2d3542cb59aa
ikeuli <m0ixbear@live.ca>
2014-06-13 12:05:24 +0900

    6/13/14 #2
    Set the default time to Asia/Seoul and changed the way database stores
    dates.

--- public_html/tracker.php
+++ public_html/tracker.php
@@ -7,16 +7,24 @@ $connect = mysql_connect("localhost", "heuncxep", "fadfjke11");
 if (!connect) { die('Connection Failed: ' . mysql_error()); } 
 mysql_select_db("heuncxep_heunjeok", $connect);
 
+date_default_timezone_set('Asia/Seoul');
+$day = date(d);
+$month = date(m);
+
 $user_info = 'INSERT INTO spending_data (
 	username, 
 	category, 
-	amount
+	amount,
+	day,
+	month
 	) 
 	VALUES 
 	(
 	"' . $fgmembersite->SanitizeForSQL($_SESSION['username']) . '", 
 	"' . $fgmembersite->SanitizeForSQL($_POST['spending_category']) . '", 
-	"' . $fgmembersite->SanitizeForSQL($_POST['amount_spent']) . '"
+	"' . $fgmembersite->SanitizeForSQL($_POST['amount_spent']) . '",
+	"' . $day . '",
+	"' . $month . '"
 	)';
 	
 if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
