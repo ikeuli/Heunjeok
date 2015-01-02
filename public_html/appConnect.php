d09d5b4b68ef8b54e26fad25aae5144a47004647
ikeuli <m0ixbear@live.ca>
2015-01-02 17:07:44 -0500

    1/2/15
    App can successfully connect in all desired ways. Could use some code
    refactoring in the future.

--- public_html/appConnect.php
+++ public_html/appConnect.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("./include/membersite_config.php");
+	session_start();
 	
 	if ($_POST['systemCall'] == "checkLogin") 
 	{
@@ -7,5 +8,36 @@
 			print "systemResult=Incorrect username or password.";
 		else
 			print "systemResult=Success!";
+	}
+	elseif ($_POST['systemCall'] == "inputStats") 
+	{
+		$connect = mysql_connect("localhost", "heuncxep", "fadfjke11"); 
+		if (!connect) { die('Connection Failed: ' . mysql_error()); } 
+		mysql_select_db("heuncxep_heunjeok", $connect);
+
+		$day = $fgmembersite->getDay();
+		$month = $fgmembersite->getMonth(1);
+		$year = $fgmembersite->getYear(1);
+
+		$user_info = 'INSERT INTO spending_data (
+			username, 
+			category, 
+			amount,
+			day,
+			month,
+			year
+			) 
+			VALUES 
+			(
+			"' . $fgmembersite->SanitizeForSQL($_SESSION['username']) . '", 
+			"' . $fgmembersite->SanitizeForSQL($_POST['category']) . '", 
+			"' . $fgmembersite->SanitizeForSQL($_POST['amount']) . '",
+			"' . $day . '",
+			"' . $month . '",
+			"' . $year . '"
+			)';
+			
+		if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
+		mysql_close($connect);
 	}
 ?>
