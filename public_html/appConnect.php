63c0039af9cdec1eedfb42741818dba709882ec4
ikeuli <m0ixbear@live.ca>
2015-01-19 13:30:06 -0500

    1/19/15
    Can get a date and memo from the app.

--- public_html/appConnect.php
+++ public_html/appConnect.php
@@ -15,28 +15,62 @@
 		if (!connect) { die('Connection Failed: ' . mysql_error()); } 
 		mysql_select_db("heuncxep_heunjeok", $connect);
 
-		$day = $fgmembersite->getDay();
-		$month = $fgmembersite->getMonth(1);
-		$year = $fgmembersite->getYear(1);
+		if(!empty($_POST['customDay']))
+			$day = (int)$_POST['customDay'];
+		else
+			$day = $fgmembersite->getDay();
+		if(!empty($_POST['customMonth']))
+			$month = (int)$_POST['customMonth'];
+		else
+			$month = $fgmembersite->getMonth(1);
+		if(!empty($_POST['customYear']))
+			$year = (int)$_POST['customYear'];
+		else
+			$year = $fgmembersite->getYear(1);
 
-		$user_info = 'INSERT INTO spending_data (
-			username, 
-			category, 
-			amount,
-			day,
-			month,
-			year
-			) 
-			VALUES 
-			(
-			"' . $fgmembersite->SanitizeForSQL($_SESSION['username']) . '", 
-			"' . $fgmembersite->SanitizeForSQL($_POST['category']) . '", 
-			"' . $fgmembersite->SanitizeForSQL($_POST['amount']) . '",
-			"' . $day . '",
-			"' . $month . '",
-			"' . $year . '"
-			)';
-			
+		if(!empty($_POST['memo']))
+		{
+			$user_info = 'INSERT INTO spending_data (
+				username, 
+				category, 
+				amount,
+				day,
+				month,
+				year,
+				memo
+				) 
+				VALUES 
+				(
+				"' . $fgmembersite->SanitizeForSQL($_SESSION['username']) . '", 
+				"' . $fgmembersite->SanitizeForSQL($_POST['category']) . '", 
+				"' . $fgmembersite->SanitizeForSQL($_POST['amount']) . '",
+				"' . $day . '",
+				"' . $month . '",
+				"' . $year . '",
+				"' . $fgmembersite->SanitizeForSQL($_POST['memo']) . '"
+				)';
+		}
+		else
+		{
+			$user_info = 'INSERT INTO spending_data (
+				username, 
+				category, 
+				amount,
+				day,
+				month,
+				year
+				) 
+				VALUES 
+				(
+				"' . $fgmembersite->SanitizeForSQL($_SESSION['username']) . '", 
+				"' . $fgmembersite->SanitizeForSQL($_POST['category']) . '", 
+				"' . $fgmembersite->SanitizeForSQL($_POST['amount']) . '",
+				"' . $day . '",
+				"' . $month . '",
+				"' . $year . '"
+				)';
+		}
+		
 		if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
 		mysql_close($connect);
 		
