821e279471c8c868da2c3a0621d787fd6a7f85ac
ikeuli <m0ixbear@live.ca>
2015-01-20 16:27:06 -0500

    1/20/15
    Put a demo of the app on the site.

---
+++ public_html/appDemo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+	<head>
+		  <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+		  <title>Heunjeok | App Demo</title>
+		  <link rel="stylesheet" type="text/css" href="/css/style.css" />
+		  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" /> 
+	</head>
+	
+	<body background="/assets/images/background.jpg">
+		<img src="/assets/images/heunjeok.png" id="logo" alt="Heunjeok logo">
+		<div id="main_content" align="center">
+			<object width="640" height="960">
+				<param name="movie" value="http://heunjeok.com/assets/flash/login.swf">
+				<embed src="http://heunjeok.com/assets/flash/login.swf" width="640" height="960">
+				</embed>
+			</object>
+		</div>
+	</body>
+</html>
